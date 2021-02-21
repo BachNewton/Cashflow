ea77f965db1efc40d91c5939835d30eed8b8941e
Kyle Hutchinson <kyle1235@gmail.com>
2021-02-21 15:07:44 -0500

    Added actions for fast track

--- src/components/ActionsTab.js
+++ src/components/ActionsTab.js
@@ -19,6 +19,10 @@ export default class ActionsTab extends React.Component {
         this.getBankLoanButtonDetails = this.getBankLoanButtonDetails.bind(this);
         this.getRequiredBankLoan = this.getRequiredBankLoan.bind(this);
         this.getPaycheckButtonDetails = this.getPaycheckButtonDetails.bind(this);
+        this.getCashflowDayButtonDetails = this.getCashflowDayButtonDetails.bind(this);
+        this.canGiveToCharity = this.canGiveToCharity.bind(this);
+        this.handleLoseAllSavings = this.handleLoseAllSavings.bind(this);
+        this.handleLostHalfOfSavings = this.handleLostHalfOfSavings.bind(this);
 
         /** @type {function(): Profession} */
         this.getProfession = props.getProfession || function () { return new Profession(); };
@@ -79,7 +83,11 @@ export default class ActionsTab extends React.Component {
 
     getCostOfCharity() {
         var profession = this.getProfession();
-        return Math.floor(0.1 * profession.getTotalIncome());
+        if (profession.inRatRace) {
+            return Math.floor(0.1 * profession.getTotalIncome());
+        }
+
+        return 100000;
     }
 
     handleCharity() {
@@ -99,9 +107,25 @@ export default class ActionsTab extends React.Component {
         this.actionTaken();
     }
 
+    handleLoseAllSavings() {
+        var profession = this.getProfession();
+        profession.savings = 0;
+        this.actionTaken();
+    }
+
+    handleLostHalfOfSavings() {
+        var profession = this.getProfession();
+        profession.savings = Math.floor(profession.savings / 2);
+        this.actionTaken();
+    }
+
     getCharityButtonDetails() {
         var profession = this.getProfession();
-        return "Are you sure want to give 10% of your income to charity? That will cost $" + this.getCostOfCharity().toLocaleString() + ". You currently have $" + profession.savings.toLocaleString() + " in savings.";
+        if (profession.inRatRace) {
+            return "Are you sure want to give 10% of your income to charity? That will cost $" + this.getCostOfCharity().toLocaleString() + ". You currently have $" + profession.savings.toLocaleString() + " in savings.";
+        }
+
+        return "Are you sure want to give $" + this.getCostOfCharity().toLocaleString() + " to charity? You currently have $" + profession.savings.toLocaleString() + " in savings.";
     }
 
     getDownsizeButtonDetails() {
@@ -117,17 +141,42 @@ export default class ActionsTab extends React.Component {
         return "Are you sure you want to collect your paycheck? Your current cashflow is $" + profession.getCashflow().toLocaleString() + ".";
     }
 
+    getCashflowDayButtonDetails() {
+        var profession = this.getProfession();
+        return "Are you sure you want to collect your Cashflow Day Income? Your current cashflow is $" + profession.getCashflow().toLocaleString() + ".";
+    }
+
+    canGiveToCharity() {
+        var profession = this.getProfession();
+        return profession.savings >= this.getCostOfCharity();
+    }
+
     render() {
         var profession = this.getProfession();
 
+        if (profession.inRatRace) {
+            return (
+                <Jumbotron>
+                    <WarningButton buttonText="Paycheck" title="Payday!" details={this.getPaycheckButtonDetails} callback={this.handlePaycheck} />
+                    <WarningButton buttonText="Doodad" title="Buy a Doodad" details="Would you like to purchase a doodad?" form={this.doodadForm} callback={this.handleDoodad} />
+                    <WarningButton buttonText="Bank Loan" title="Take a Bank Loan" details={this.getBankLoanButtonDetails} callback={this.handleBankLoan} />
+                    <WarningButton buttonText="Baby" title="Have a Baby" details="Are you sure you want to have a baby?" callback={this.handleBaby} getEnabled={() => profession.children < 3} />
+                    <WarningButton buttonText="Charity" title="Give to Charity" details={this.getCharityButtonDetails} callback={this.handleCharity} getEnabled={this.canGiveToCharity} />
+                    <WarningButton buttonText="Downsized" title="Downsize" details={this.getDownsizeButtonDetails} callback={this.handleDownsize} />
+                </Jumbotron>
+            );
+        }
+
         return (
             <Jumbotron>
-                <WarningButton buttonText="Paycheck" title="Payday!" details={this.getPaycheckButtonDetails} callback={this.handlePaycheck} />
-                <WarningButton buttonText="Doodad" title="Buy a Doodad" details="Would you like to purchase a doodad?" form={this.doodadForm} callback={this.handleDoodad} />
-                <WarningButton buttonText="Bank Loan" title="Take a Bank Loan" details={this.getBankLoanButtonDetails} callback={this.handleBankLoan} />
-                <WarningButton buttonText="Baby" title="Have a Baby" details="Are you sure you want to have a baby?" callback={this.handleBaby} getEnabled={() => profession.children < 3} />
-                <WarningButton buttonText="Charity" title="Give to Charity" details={this.getCharityButtonDetails} callback={this.handleCharity} />
-                <WarningButton buttonText="Downsized" title="Downsize" details={this.getDownsizeButtonDetails} callback={this.handleDownsize} />
+                <div style={{ textAlign: "center", fontWeight: "bold", marginBottom: "1rem", fontSize: "1.5rem" }}>
+                    Actions in <span style={{ color: "lime" }}>Fast Track</span>!
+                </div>
+                <WarningButton buttonText="Cashfow Day" title="FREE MONEY!" details={this.getCashflowDayButtonDetails} callback={this.handlePaycheck} />
+                <WarningButton buttonText="Charity" title="So Very Generous" details={this.getCharityButtonDetails} callback={this.handleCharity} getEnabled={this.canGiveToCharity} />
+                <WarningButton buttonText="Tax Audit" title="You've Been Audited" details="Are you sure you've been audited? That will cost you half of your entire savings." callback={this.handleLostHalfOfSavings} />
+                <WarningButton buttonText="Divorce" title="Not Meant To Be" details="Are you sure you want a divorce? That will cost you all of your savings." callback={this.handleLoseAllSavings} />
+                <WarningButton buttonText="Lawsuit" title="You've Been Sued" details="Are you sure you've been sued? That will cost you half of your entire savings." callback={this.handleLostHalfOfSavings} />
             </Jumbotron>
         );
     }
